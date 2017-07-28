e641aafc985df39da79e6f4bad9cb530db567cf6
Aaron Stanek <nope>
2017-07-28 10:45:28 -0700

    done with core functionality of software trigger system

--- run_softwareTrigger.py
+++ run_softwareTrigger.py
@@ -11,3 +11,19 @@ def getInputFiles(path):
         if os.path.isfile(path+x):
             ou.append(x)
     return ou
+
+def main(inpath,outpath,lev,direction):
+    #lev is a dict
+    #direction is a string
+    #see softwareTrigger.py::softwareTrigger for more information
+    st = softwareTrigger.softwareTrigger()
+    st.lev = lev
+    st.direction = direction
+    files = getInputFiles(inpath)
+    for f in files:
+        [osc_meta,ev_list] = clread.read(inpath+f)
+        ev_list_scanned = st.scanList(ev_list)
+        clwrite(outpath,osc_meta,ev_list_scanned)
+        del(osc_meta)
+        del(ev_list)
+        del(ev_list_scanned)
