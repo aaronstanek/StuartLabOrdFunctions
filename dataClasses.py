bba4da94b958553cf2c5d4b6a3b9376c42657e97
Aaron Stanek <nope>
2017-08-14 14:57:48 -0700

    added getMaxes function

--- dataClasses.py
+++ dataClasses.py
@@ -5,3 +5,9 @@ class data_event:
     def fromDict(self,d):
         self.meta = d["EV_META"]
         self.data = d["DATA"]
+    def getMaxes(self):
+        u = list(self.data)
+        ou = dict()
+        for chan in u:
+            ou[chan] = max(self.data[chan])
+        return ou
