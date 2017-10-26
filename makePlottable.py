ac20eab33ddbada859047d589bda5f4e9824b1c0
Aaron Stanek <nope>
2017-10-26 16:17:16 -0700

    integrated plotChop function

--- makePlottable.py
+++ makePlottable.py
@@ -53,7 +53,8 @@ def makePlottable(data,get1,get2,**options):
             ran["ymin"] = yVal
         if yVal>ran["ymax"]:
             ran["ymax"] = yVal
-        ou.append([xVal,yVal])
+        if plotChop(xVal,yVal,options)==False:
+            ou.append([xVal,yVal])
     return ou,ran
 
 def makePlottable_single(data,get1,**options):
